24ba0411b5272ab2bb00c8dc8240890df646019a
wywdelpy <e4orez@gmail.com>
2022-08-13 14:49:12 +0600

    add some comments

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,6 +2,12 @@ from aiogram import types
 from loader import dp
 
 
+# ловит в хендлер в фото
+# @dp.message_handler(content_types = types.ContentTypes.PHOTO)
+
 @dp.message_handler()
 async def bot_echo(message: types.Message):
     await message.answer(f'Cам ты {message.text}')
+
+    # оформить, как ответ на конретное сообщение
+    # await message.reply(f'Cам ты {message.text}')
