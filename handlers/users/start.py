1a1e28ba134d920cba05fcc3cb9a75697ff9e148
hitman0797 <paxan0393@gmail.com>
2022-08-15 23:38:00 +0600

    emoji 2

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -5,6 +5,7 @@ from loader import dp
 # достаем menu из дир-и delpy_bot -> keyboards -> default
 from keyboards.default import menu
 
+emoji = = u'\U0001F4A8'
 
 # @dp.message_handler ловит только комманду /start
 @dp.message_handler(CommandStart())
@@ -14,4 +15,4 @@ async def bot_start(message: types.Message):
     # при вводе /start также появляется menu с кнпоками, благодаря
     # reply_markup=menu
     # menu указывается в keyboards->default->menu
-    await message.answer(f'<b>Привет</b> :clown:, {message.from_user.full_name}!', reply_markup=menu)
+    await message.answer(f'<b>Привет</b>,{emoji} {message.from_user.full_name}!', reply_markup=menu)
